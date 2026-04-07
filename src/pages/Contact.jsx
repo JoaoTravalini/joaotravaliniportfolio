7382ff51eab331d5bd52634079ab0546da378861
João Travalini <joao.m.travalini@gmail.com>
2026-04-07 14:38:56 -0300

    add keys emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: ''})
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.sendForm(
+   
+    )
+  };
+
   const handleFocus = () => {};
   const handleBlur = () => {};
 
@@ -13,7 +27,7 @@ const Contact = () => {
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Entre em contato</h1>
 
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
           <label className="text-black-500 font-semibold">
             Nome
             <input type="text" name="name" className="input" placeholder="Nome" required value={form.name} onChange={handleChange} onFocus={handleFocus} onBlur={handleBlur} />
