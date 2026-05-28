718997d6ef39cffa28235601ad17cac4bfe46858
João Travalini <joao.m.travalini@gmail.com>
2026-05-27 21:08:50 -0300

    wtvr

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,6 @@
+import vail from './vail.png'
+import eletrodenis from './eletrodenis.png'
+import asimovjr from './asimovjr.png'
 import hero from './hero.jpg'
 import meta from './meta.png'
 import shopify from './shopify.png'
@@ -6,6 +9,9 @@ import tesla from './tesla.png'
 import logo from './logo.svg'
 
 export {
+    vail,
+    eletrodenis,
+    asimovjr,
     hero,
     meta,
     shopify,
